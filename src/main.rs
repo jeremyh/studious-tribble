67d9ae7cc87a87d0c85b4793ae8de40478d7d010
Jeremy Hooke <jez@stulk.com>
2020-05-01 21:19:41 +1000

    Chapter 1: output ppm

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::fs::File;
+use std::io::BufWriter;
+use std::io::Write as IoWrite;
+use std::path::Path;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = Path::new("image.ppm");
+    let mut o = BufWriter::new(File::create(&path)?);
+
+    let (width, height) = (200, 100);
+    writeln!(
+        &mut o,
+        "P3\n{nx} {ny}\n255",
+        nx = width,
+        ny = height
+    )?;
+
+    for j in (0..height).rev() {
+        for i in 0..width {
+            let r = i as f32 / width as f32;
+            let g = j as f32 / height as f32;
+            let b = 0.2;
+
+            let ir = (255.99 * r) as i32;
+            let ig = (255.99 * g) as i32;
+            let ib = (255.99 * b) as i32;
+
+            writeln!(
+                &mut o,
+                "{:?} {:?} {:?}",
+                ir, ig, ib
+            )?;
+        }
+    }
+
+    Ok(())
+}
